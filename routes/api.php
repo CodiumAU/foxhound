ae50ece2b127a66545d142ce3ecdd602de491f66
Jason Lewis <jason.lewis1991@gmail.com>
2023-11-29 15:25:40 +1030

    refactor: remove old controllers

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,5 @@
 use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
-Route::get('channels/mail', [Controllers\Channels\MailController::class, 'index']);
-Route::get('channels/mail/{uuid}/html', Controllers\Channels\MailRenderController::class);
 Route::get('channels/{channel}/messages', [Controllers\ChannelMessageController::class, 'index']);
 Route::get('channels/{channel}/{uuid}/html', Controllers\ChannelHtmlController::class);
