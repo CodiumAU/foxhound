fe81d6ec3035e5a52b901a8333cae97be0e1698d
Jason Lewis <jason.lewis1991@gmail.com>
2023-12-04 19:43:28 +1030

    fix: use absolute paths to favicons in published public directory

--- resources/js/components/app/AppSidebar.vue
+++ resources/js/components/app/AppSidebar.vue
@@ -31,14 +31,12 @@ import { storeToRefs } from 'pinia'
 import { computed } from 'vue'
 import { useFavicon } from '@vueuse/core'
 import AppSidebarChannelItem from './AppSidebarChannelItem.vue'
-import FaviconRead from '../../../img/read.png'
-import FaviconUnread from '../../../img/unread.png'
 
 const channelsStore = useChannelsStore()
 const { channels } = storeToRefs(channelsStore)
 
 await channelsStore.getChannels()
 
-const favicon = computed(() => channels.value.some((channel) => channel.unread_messages_count > 0) ? FaviconUnread : FaviconRead)
+const favicon = computed(() => channels.value.some((channel) => channel.unread_messages_count > 0) ? '/vendor/foxhound/img/unread.png' : '/vendor/foxhound/img/read.png')
 useFavicon(favicon)
 </script>
