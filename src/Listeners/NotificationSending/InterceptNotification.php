d4023996fb9a535252191034653bcfcf4bb309ac
Jason Lewis <jason.lewis1991@gmail.com>
2025-05-06 16:30:59 +0930

    fix: still dispatch notification sent event after intercepting

--- src/Listeners/NotificationSending/InterceptNotification.php
+++ src/Listeners/NotificationSending/InterceptNotification.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Str;
 use Foxhound\ChannelManager;
 use InvalidArgumentException;
 use Foxhound\Contracts\Storage;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Notifications\Events\NotificationSent;
 use Illuminate\Notifications\Events\NotificationSending;
 use Illuminate\Contracts\Config\Repository as ConfigRepository;
 
@@ -19,6 +21,7 @@ class InterceptNotification
     public function __construct(
         protected ChannelManager $manager,
         protected ConfigRepository $config,
+        protected Dispatcher $events,
         protected Storage $storage
     ) {
     }
@@ -46,6 +49,12 @@ class InterceptNotification
 
             // Save the manifest after the driver has run any additional logic for the interception.
             $this->storage->saveManifest($manifest);
+
+            // Continue to dispatch the notification sent event after we intercepted the notification. We can then
+            // return false to stop the notification from actually sending as Foxhound has intercepted it.
+            $this->events->dispatch(
+                new NotificationSent($event->notifiable, $event->notification, $event->channel)
+            );
 
             return false;
         } catch (InvalidArgumentException) {
